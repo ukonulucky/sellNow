5ceba56c7ce6e619a2a22b9a08d389b4febac0d6
ukonulucky <ukonulucky@gmail.com>
2022-09-03 21:58:13 +0000

    ReactNative

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("app running")
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello word now44!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
