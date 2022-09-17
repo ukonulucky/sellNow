0e73b3d69fe0bd02d9c20eec3136b0d05fda71e8
ukonulucky <ukonulucky@gmail.com>
2022-09-17 14:42:22 +0000

    Create a new Expo app

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import React, { useState , useCallback} from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Home from "./screen/Home"
 import About from "./screen/About"
+import { FontAwesome5 } from "@expo/vector-icons"
 
+// the tap navigator is used to navigate between different screens in the app
+const Tap = createBottomTabNavigator()
 
-const Stack = createNativeStackNavigator()
  export default function App() {
    return (
      <NavigationContainer>
-       <Stack.Navigator initialRouteName="About">
-         <Stack.Screen name="Home" component={Home} />
-         <Stack.Screen name="About" component={About} />
-       </Stack.Navigator>
-      
+       <Tap.Navigator initialRouteName="About" screenOptions={({ route }) => ({
+         tabBarIcon: ({ focused, size, color}) => {
+           let iconName
+           if (route.name === "About") {
+            
+             iconName = "twitter"
+             color = focused ? "purple" : "crimpson"
+             size = focused ? 30 : 20
+           } else if (route.name === "Home") {
+             
+             iconName = "youtube"
+             color = focused ? "purple" : "crimpson"
+             size = focused ? 30 : 20
+           } 
+           return <FontAwesome5 name={iconName} size={size} color={ color} />
 
+         }
+       })}>
+         <Tap.Screen name="Home" component={Home} />
+         <Tap.Screen name="About" component={About} />
+       </Tap.Navigator>
      </NavigationContainer>
   )
 } 
